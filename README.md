fa557d01313f1c4f4f69d2f041c0c2755490ae24
Anna Kuznetsova and Mick Sexton <anna+mick@>
2017-08-28 11:17:13 -0700

    add firebase api key and sample scores

--- README.md
+++ README.md
@@ -26,3 +26,14 @@ Before running the tests make sure you are serving the app via `ng serve`.
 ## Further help
 
 To get more help on the Angular CLI use `ng help` or go check out the [Angular CLI README](https://github.com/angular/angular-cli/blob/master/README.md).
+
+###API Key for Firebase
+
+export var masterFirebaseConfig = {
+  apiKey: "AIzaSyDrUl4_IAzL_Xv3KGHo3wnir7x3RF_A8qo",
+  authDomain: "bubble-burst-7169a.firebaseapp.com",
+  databaseURL: "https://bubble-burst-7169a.firebaseio.com",
+  projectId: "bubble-burst-7169a",
+  storageBucket: "",
+  messagingSenderId: "833237462114"
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,22 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 import { routing } from './app.routing';
 
+import { masterFirebaseConfig } from './api-keys';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+
 import { AppComponent } from './app.component';
 import { WelcomeComponent } from './welcome/welcome.component';
 import { GameComponent } from './game/game.component';
 import { AboutComponent } from './about/about.component';
 import { HighscoresComponent } from './highscores/highscores.component';
+
+export const firebaseConfig = {
+  apiKey: masterFirebaseConfig.apiKey,
+  authDomain: masterFirebaseConfig.authDomain,
+  databaseURL: masterFirebaseConfig.databaseURL,
+  storageBucket: masterFirebaseConfig.storageBucket
+};
 
 @NgModule({
   declarations: [
