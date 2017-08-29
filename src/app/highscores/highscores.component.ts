496c059616a22a55ac12ac4d5bcc0681d9cd7313
Mick Sexton <lacrookedbeat@hotmail.com>
2017-08-29 14:12:25 -0700

    add sorting of high scores, pushing into array

--- src/app/highscores/highscores.component.ts
+++ src/app/highscores/highscores.component.ts
@@ -12,17 +12,37 @@ import { ScoreService } from '../score.service';
 
 export class HighscoresComponent implements OnInit {
 
-  scores: FirebaseListObservable<any[]>;
+  scores;
+  newScores: Score[] = [];
+  orderedScores: Score[] = [];
 
   constructor(private scoreService: ScoreService) { }
 
   ngOnInit() {
-    this.scores = this.scoreService.getScores();
+    this.scores = this.scoreService.getScores().subscribe(scores => {
+      scores.forEach(score => {
+        console.log(score.name + " " + score.points);
+        let newScore = new Score(score.name, score.points);
+        console.log(newScore.name + " " + newScore.points);
+        this.newScores.push(newScore);
+      });
+      console.log(this.newScores.toString());
+      // this.newScores = this.newScores.sort();
+      // this.newScores = _.sortBy(this.newScores, 'points');
+      this.orderedScores = this.newScores.sort(function(obj1, obj2) {
+        return obj1.points - obj2.points;
+      });
+      this.orderedScores = this.orderedScores.reverse();
+    });
   }
+
 
   submitNewScore(name, points) {
     var newScore: Score = new Score(name, points);
     this.scoreService.addScore(newScore);
+    window.location.reload();
   }
+
+
 
 }
